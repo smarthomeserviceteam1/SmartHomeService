91d7736dab0de827b59d85eafa8973849ea0d09d
tonymazn <tonymazn@gamil.com>
2014-10-23 08:35:48 -0600

    Add back missing file

---
+++ RESTService/SmartHome.ViewModel/IndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.ViewModel
+{
+    public class IndexViewModel
+    {
+
+        public string SessionID { get; set; }
+        public string SecretKey{get;set;}
+        public string UserID {get;set;}
+        public string Pin { get; set; }
+        public string Hash { get; set;}
+    }
+
+}
